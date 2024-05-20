9f7205ded68da6b1835dca9c866649ec4f50eb21
Ilya <ixlax@mail.ru>
2024-05-21 00:43:03 +0300

    WeatherEvery3H
    Показывает погоду на будущее

--- app/src/main/java/com/example/weatherapplication/MainPageActivity.java
+++ app/src/main/java/com/example/weatherapplication/MainPageActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.textservice.TextInfo;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,7 +37,12 @@ public class MainPageActivity extends AppCompatActivity {
     TextView pressureWeather;
     TextView humidityWeather;
     TextView windSpeedWeather;
+    TextView tempAfter3H;
+    TextView tempAfter6H;
+    TextView tempAfter9H;
+    TextView tempAfter12H;
     String town;
+    double lat,lon; // координаты
 
     DecimalFormat dfOne = new DecimalFormat("#");
     DecimalFormat dfTwo = new DecimalFormat("#.##");
@@ -57,6 +63,12 @@ public class MainPageActivity extends AppCompatActivity {
         humidityWeather = findViewById(R.id.humidity);
         windSpeedWeather = findViewById(R.id.speed);
         pressureWeather = findViewById(R.id.pressure);
+
+        tempAfter3H = findViewById(R.id.tempAfter3);
+        tempAfter6H = findViewById(R.id.tempAfter6);
+        tempAfter9H = findViewById(R.id.tempAfter9);
+        tempAfter12H = findViewById(R.id.tempAfter12);
+
         city1 = findViewById(R.id.city1);
         city2 = findViewById(R.id.city2);
 
@@ -98,6 +110,7 @@ public class MainPageActivity extends AppCompatActivity {
 
     public void getWeatherInfo(){
         String URL = "https://api.openweathermap.org/data/2.5/weather?q=" + town +"&appid=58a88bc8fa24706f76fd408cbbd6e0ca&units=metric";
+
         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -120,6 +133,11 @@ public class MainPageActivity extends AppCompatActivity {
                     double humidity = jsonObjectMain.getDouble("humidity");
                     double pressure = jsonObjectMain.getDouble("pressure");
 
+                    JSONObject jsonObjectCoord = jsonObjectResponse.getJSONObject("coord"); // получаем корды
+                    lat = jsonObjectCoord.getDouble("lat");
+                    lon = jsonObjectCoord.getDouble("lon");
+                    getWeatherInfoAfter12H(lat,lon);
+
                     JSONObject jsonObjectWind = jsonObjectResponse.getJSONObject("wind"); // Заходим в папку с ветром
                     double speedWind = jsonObjectWind.getDouble("speed"); // Получаем скорость ветра
 
@@ -136,6 +154,7 @@ public class MainPageActivity extends AppCompatActivity {
                     humidityWeather.setText("Влажность: " + humidityWea + "%");
 
                     city2.setText(town);
+
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
                 }
@@ -148,5 +167,68 @@ public class MainPageActivity extends AppCompatActivity {
         });
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(stringRequest);
+    }
+
+    public void getWeatherInfoAfter12H(double lat,double lon){
+        String URL5days = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=58a88bc8fa24706f76fd408cbbd6e0ca";
+        StringRequest stringRequestTwo = new StringRequest(Request.Method.POST, URL5days, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                String tempAfter3 = "";
+                String tempAfter6 = "";
+                String tempAfter9 = "";
+                String tempAfter12 = "";
+
+                try {
+
+                    JSONObject jsonObjectResponse = new JSONObject(response);
+                    JSONArray jsonArrayWeatherList = jsonObjectResponse.getJSONArray("list"); // зашли в лист
+
+                    // Данные через 3 часа
+                    JSONObject jsonObjectWeatherTemp1 = jsonArrayWeatherList.getJSONObject(1);
+                    JSONObject jsonObjectMain1 = jsonObjectWeatherTemp1.getJSONObject("main");
+                    double temp1 = jsonObjectMain1.getDouble("temp") - 272;
+
+// Данные через 6 часа
+                    JSONObject jsonObjectWeatherTemp2 = jsonArrayWeatherList.getJSONObject(2);
+                    JSONObject jsonObjectMain2 = jsonObjectWeatherTemp2.getJSONObject("main");
+                    double temp2 = jsonObjectMain2.getDouble("temp") - 272;
+
+// Данные через 9 часа
+                    JSONObject jsonObjectWeatherTemp3 = jsonArrayWeatherList.getJSONObject(3);
+                    JSONObject jsonObjectMain3 = jsonObjectWeatherTemp3.getJSONObject("main");
+                    double temp3 = jsonObjectMain3.getDouble("temp") - 272;
+
+// Данные через 12 часов
+                    JSONObject jsonObjectWeatherTemp4 = jsonArrayWeatherList.getJSONObject(4);
+                    JSONObject jsonObjectMain4 = jsonObjectWeatherTemp4.getJSONObject("main");
+                    double temp4 = jsonObjectMain4.getDouble("temp") - 272;
+
+// Установка значений
+                    tempAfter3 += dfOne.format(temp1);
+                    tempAfter6 += dfOne.format(temp2);
+                    tempAfter9 += dfOne.format(temp3);
+                    tempAfter12 += dfOne.format(temp4);
+
+                    tempAfter3H.setText(tempAfter3 + "°");
+                    tempAfter6H.setText(tempAfter6 + "°");
+                    tempAfter9H.setText(tempAfter9 + "°");
+                    tempAfter12H.setText(tempAfter12 + "°");
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                //Log.d("response",response);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                //Toast.makeText(getApplicationContext(), volleyError.toString().trim(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueueTwo = Volley.newRequestQueue(getApplicationContext());
+        requestQueueTwo.add(stringRequestTwo);
     }
 }
